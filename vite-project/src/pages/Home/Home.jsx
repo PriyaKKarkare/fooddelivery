2bf3de02622b971933dbafd4c3add091cf980963
Priya Kashinath Karkare <priya9930688736@gmail.com>
2024-09-13 07:58:45 +0530

    foodDelivery

---
+++ vite-project/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react'
+import './Home.css'
+import Header from '../../components/Header/Header'
+import Exploremenu from '../../components/ExploreMenu/Exploremenu'
+
+const Home = () => {
+  const [category, setCategory] = useState("All");
+
+  return (
+    <div>
+      <Header />
+      <Exploremenu category={category} setCategory={setCategory} />
+    </div>
+  )
+}
+
+export default Home
